c411f01e2357def27c06c75d6599c3fce59a7969
medantechno <admin@medantechno.com>
2022-01-18 11:02:29 +0700

    awal

--- application/views/get_barcode.php
+++ application/views/get_barcode.php
@@ -19,10 +19,7 @@
       echo "<center><b>Rp.".rupiah($barang->harga_retail)."</b></center>";
       echo "<img src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'> &nbsp;&nbsp;&nbsp;&nbsp;";
 
-      if($i%3==0)
-      {
-        echo "<br>";
-      }
+      
     }
     
     echo"<br><br>";
