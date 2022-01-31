e482c3edd02cdcf26c463035e571555d1346acba
medantechno <admin@medantechno.com>
2022-01-31 11:45:05 +0700

    print

--- application/views/get_barcode.php
+++ application/views/get_barcode.php
@@ -34,7 +34,7 @@
         $spasi = "&nbsp&nbsp&nbsp&nbsp&nbsp";
       }
       echo "<div style=''>";
-      echo " <img style='padding:1mm;width:40mm; margin:3mm text-align:center;' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
+      echo " <img style='padding:1mm;width:40mm; margin:2mm; text-align:center;' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
       echo "</div>";
       
     }
