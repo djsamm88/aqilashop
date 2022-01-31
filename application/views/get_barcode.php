40e775477ac1c61147116bd01971aa79ecaf5204
medantechno <admin@medantechno.com>
2022-01-31 10:13:52 +0700

    print

--- application/views/get_barcode.php
+++ application/views/get_barcode.php
@@ -18,7 +18,7 @@
 
       //echo "<center><b>Rp.".rupiah($barang->harga_retail)."</b></center>";
       echo "<center>";
-      echo " &nbsp;&nbsp;&nbsp;&nbsp;<img style='margin-top:10px;' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'> &nbsp;&nbsp;&nbsp;";
+      echo " &nbsp;&nbsp;&nbsp;&nbsp;<img style='margin-top:20px;' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
       echo "</center>";
       
     }
