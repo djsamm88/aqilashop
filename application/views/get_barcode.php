67445a84aad216b39ce35212ea440bf166d49819
medantechno <admin@medantechno.com>
2022-02-03 10:49:43 +0700

    printer ke 2

--- application/views/get_barcode.php
+++ application/views/get_barcode.php
@@ -34,7 +34,7 @@
         $spasi = "&nbsp&nbsp&nbsp&nbsp&nbsp";
       }
       echo "<div style='text-align:center;'>";
-      echo " <img style='padding-top:0mm;width:37mm; height:17mm; margin:0mm; ' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
+      echo " <img style='padding-top:0mm;width:37mm; height:15mm; margin:0mm; ' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
       echo "</div>";
       
     } 
