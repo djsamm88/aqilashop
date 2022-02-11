14e2a08bc6593dd635df72c7d2983428d6ff895b
medantechno <admin@medantechno.com>
2022-02-11 10:32:12 +0700

    fix

--- application/views/get_barcode.php
+++ application/views/get_barcode.php
@@ -37,9 +37,10 @@
         
       }
 
-      echo " <img style='text-align:center; padding-top:0mm;height:15 mm; position:relative; margin-bottom:0mm;margin-top:1mm; ' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
-      
-      
+      echo "<div style='text-align:center; '>";
+      echo " <img style='padding-top:0mm;height:15 mm; position:relative; margin-bottom:0mm;margin-top:1mm; ' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
+      echo "</div>";
+
     } 
     
   }
