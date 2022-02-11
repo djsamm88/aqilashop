66024d6a69c0a13aba5fe471108d026d3428165e
medantechno <admin@medantechno.com>
2022-02-11 11:42:09 +0700

    fix

--- application/views/get_barcode.php
+++ application/views/get_barcode.php
@@ -41,8 +41,8 @@
 
       echo "<div class='col-xs-4' style='padding:20px'>";
       
-      echo " <img style='padding-top:0mm;width:80%;' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
-      echo "<br><span style='position:absolute;margin-top:-10px'>Rp.".rupiah($barang->harga_retail)."</span>";
+      echo " <img style='padding-top:0mm;width:90%;' src='".base_url()."assets/barcode/barcode.php?codetype=Code128&size=40&text=$nama_barang&print=true'>";
+      echo "<br><span style='position:absolute;margin-top:-10px'><small>".rupiah($barang->harga_retail)."</small></span>";
 
      echo "</div>";
 
